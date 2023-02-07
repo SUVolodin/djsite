5f728af09996d1c149474c3a443571a6bc254f5c
Stanislav Volodin <volodin.stas87@gmail.com>
2023-02-07 21:26:09 +0300

    Создание модели Stars и выполнение миграции

--- filmstars/filmstars/urls.py
+++ filmstars/filmstars/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from filmstars import settings
 from stars.views import index, categories, pageNotFound
 from django.urls import include
 
@@ -23,5 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('stars.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 handler404 = pageNotFound
